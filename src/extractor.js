7a8a49bfa1d2a00ff7f158d133eb26d3094e979c
kubosho <ta2@o2p.jp>
2017-09-23 11:40:03 +0900

    style: add prettier

--- src/extractor.js
+++ src/extractor.js
@@ -3,32 +3,35 @@ const util = require('util')
 const htmlparser = require('htmlparser2')
 
 class Extractor {
-    async extractClassNames (filePath) {
-        const classNames = [];
-        let content = ''
+  async extractClassNames(filePath) {
+    const classNames = []
+    let content = ''
 
-        try {
-            const readFile = util.promisify(fs.readFile)
-            content = await readFile(filePath, 'utf8')
-        } catch (error) {
-            throw new Error(error)
-        }
-
-        const parser = new htmlparser.Parser({
-            onopentag: (name, attrs = {}) => {
-                if (!(attrs && attrs.class)) {
-                    return
-                }
-
-                classNames.push(attrs.class)
-            }
-        }, {decodeEntities: true})
-
-        parser.write(content)
-        parser.end()
-
-        return classNames
+    try {
+      const readFile = util.promisify(fs.readFile)
+      content = await readFile(filePath, 'utf8')
+    } catch (error) {
+      throw new Error(error)
     }
+
+    const parser = new htmlparser.Parser(
+      {
+        onopentag: (name, attrs = {}) => {
+          if (!(attrs && attrs.class)) {
+            return
+          }
+
+          classNames.push(attrs.class)
+        },
+      },
+      { decodeEntities: true },
+    )
+
+    parser.write(content)
+    parser.end()
+
+    return classNames
+  }
 }
 
 module.exports = Extractor
