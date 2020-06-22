8fe03cd493726f05d0cb3ce16d6f657a556bf8cb
Shota Kubota <ta2@o2p.jp>
2020-06-22 21:57:06 +0900

    chore(extension): add type annotation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
+import { ExtensionContext } from 'vscode';
 import { runCSSExtractor } from './commands';
 
-export function activate(context) {
+export function activate(context: ExtensionContext) {
   context.subscriptions.push(runCSSExtractor());
 }
 
