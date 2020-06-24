764e49558a765f9adcefc2ca3dd9adc0769d4fb4
Shota Kubota <ta2@o2p.jp>
2020-06-24 23:53:16 +0900

    feat(commands): add dispose function

--- src/commands.ts
+++ src/commands.ts
@@ -1,17 +1,18 @@
+import { Nullable, isNull } from 'option-t/lib/Nullable/Nullable';
 import { isUndefined } from 'option-t/lib/Undefinable/Undefinable';
 import { window as vscodeWindow, workspace as vscodeWorkspace } from 'vscode';
 import { Extractor } from './extractor';
 import { Formatter } from './formatter';
 
-const extractor = new Extractor();
-const formatter = new Formatter();
+let extractor: Nullable<Extractor> = new Extractor();
+let formatter: Nullable<Formatter> = new Formatter();
 
 const supportedFormats = ['html'];
 
-export async function runCSSExtractor(): Promise<void> {
+export async function runCSSExtractor(): Promise<{ dispose: () => void }> {
   const editor = vscodeWindow.activeTextEditor;
-  if (isUndefined(editor)) {
-    return;
+  if (isUndefined(editor) || isNull(extractor) || isNull(formatter)) {
+    return { dispose };
   }
 
   const document = editor.document;
@@ -37,4 +38,13 @@ export async function runCSSExtractor(): Promise<void> {
       language: 'css',
     }),
   );
+
+  return {
+    dispose,
+  };
+}
+
+function dispose(): void {
+  extractor = null;
+  formatter = null;
 }
