dcd074bd57ce5af5328183e04c21e7d3ebc251b0
Shota Kubota <ta2@o2p.jp>
2020-06-24 23:40:17 +0900

    refactor(formatter): add type annotation

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,15 +1,15 @@
 import { format as sourceFormat } from 'prettier';
 
 export class Formatter {
-  removeDuplicatesSelector(selectors) {
+  removeDuplicatesSelector(selectors: Array<string>): Array<string> {
     return [...new Set(selectors)];
   }
 
-  convertSelectorsToRulesets(selectors) {
+  convertSelectorsToRulesets(selectors: Array<string>): string {
     return selectors.map((selector) => `${selector}{}`).join(' ');
   }
 
-  format(source) {
+  format(source: string): string {
     return sourceFormat(source, { parser: 'postcss' });
   }
 }
