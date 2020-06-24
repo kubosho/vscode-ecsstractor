f9ba4e2b902dc3c88943488c5ccc971aa58d83c5
Shota Kubota <ta2@o2p.jp>
2020-06-24 23:37:24 +0900

    refactor(extractor): add type annotation

--- src/extractor.ts
+++ src/extractor.ts
@@ -1,16 +1,19 @@
 import { Parser as HtmlParser } from 'htmlparser2';
+import { Handler } from 'htmlparser2/lib/Parser';
 
-function createParser(callbacks) {
+function createParser(callbacks: Partial<Handler>) {
   return new HtmlParser(callbacks, { decodeEntities: true });
 }
 
 export class Extractor {
-  extractClassSelectors(content) {
-    const selectors = [];
+  extractClassSelectors(content: string): Array<string> {
+    const selectors: Array<string> = [];
 
     const parser = createParser({
-      onopentag: (name, attrs) => {
-        if (!(attrs && attrs.class)) return;
+      onopentag: (_name, attrs) => {
+        if (!(attrs && attrs.class)) {
+          return;
+        }
         const className = attrs.class.replace(/ /g, '.');
         selectors.push(`.${className}`);
       },
@@ -21,12 +24,14 @@ export class Extractor {
     return selectors;
   }
 
-  extractIDSelectors(content) {
-    const selectors = [];
+  extractIDSelectors(content: string): Array<string> {
+    const selectors: Array<string> = [];
 
     const parser = createParser({
-      onopentag: (name, attrs) => {
-        if (!(attrs && attrs.id)) return;
+      onopentag: (_name, attrs) => {
+        if (!(attrs && attrs.id)) {
+          return;
+        }
         selectors.push(`#${attrs.id}`);
       },
     });
