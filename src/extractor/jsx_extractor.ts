41831f4b0fc9f212a0f22256d22565ea1fc54d6f
Shota Kubota <ta2@o2p.jp>
2021-09-10 23:43:09 +0900

    refactor(jsx_extractor): use estraverse for extract selectors

--- src/extractor/jsx_extractor.ts
+++ src/extractor/jsx_extractor.ts
@@ -1,115 +1,59 @@
 import * as esprima from 'esprima';
-import {
-  ExportNamedDeclaration,
-  Expression,
-  FunctionDeclaration,
-} from 'estree';
-import { isNotNullAndUndefined } from 'option-t/lib/Maybe/Maybe';
+import * as estraverse from 'estraverse';
+import { Node } from 'estree';
 
-import { JSXElement, JSXText } from '../../typings/esprima_extend';
-import {
-  isClassName,
-  isExportNamedDeclaration,
-  isFunctionDeclaration,
-  isId,
-  isJSXElement,
-  isReturnStatement,
-} from '../utils';
+import { JSXElement } from '../../typings/esprima_extend';
+import { isClassName, isId, isJSXElement } from '../utils';
 
 import { Extractor } from './extractor';
 
 export class JsxExtractor implements Extractor {
-  private _classNames: string[];
-  private _ids: string[];
-
-  constructor() {
-    this._classNames = [];
-    this._ids = [];
-  }
-
   extractClassName(contents: string): string[] {
-    const { body } = esprima.parseModule(contents, { jsx: true });
+    const result: string[] = [];
 
-    const source = getJSXElements([
-      ...getFunctionDeclarations(body.filter(isExportNamedDeclaration)),
-      ...body.filter(isFunctionDeclaration),
-    ]);
-
-    source.forEach((src) => {
-      if (isJSXElement(src)) {
-        this._extractClassName([src]);
-      }
+    const ast = esprima.parseModule(contents, { jsx: true });
+    estraverse.traverse(ast, {
+      enter: (node: Node | JSXElement) => {
+        if (isJSXElement(node)) {
+          result.push(...this._extractClassName(node));
+        }
+      },
+      fallback: 'iteration',
     });
 
-    return this._classNames;
+    return result;
   }
 
   extractId(contents: string): string[] {
-    const { body } = esprima.parseModule(contents, { jsx: true });
+    const result: string[] = [];
+    const ast = esprima.parseModule(contents, { jsx: true });
 
-    const source = getJSXElements([
-      ...getFunctionDeclarations(body.filter(isExportNamedDeclaration)),
-      ...body.filter(isFunctionDeclaration),
-    ]);
-
-    source.forEach((src) => {
-      if (isJSXElement(src)) {
-        this._extractId([src]);
-      }
+    estraverse.traverse(ast, {
+      enter: (node: Node | JSXElement) => {
+        if (isJSXElement(node)) {
+          result.push(...this._extractId(node));
+        }
+      },
+      fallback: 'iteration',
     });
 
-    return this._ids;
+    return result;
   }
 
-  private _extractClassName(elements: (JSXElement | JSXText)[]): void {
-    getPartialPropertyOfElements(elements).forEach(
-      ({ children, openingElement: { attributes } }) => {
-        attributes
-          .filter(isClassName)
-          .map(({ value }) => `${value.value}`.replace(/ /g, '.'))
-          .forEach((className) => this._classNames.push(`.${className}`));
+  private _extractClassName(element: JSXElement): string[] {
+    const { openingElement } = element;
+    const { attributes } = openingElement;
 
-        this._extractClassName(children);
-      },
-    );
+    return attributes
+      .filter(isClassName)
+      .map(({ value }) => `${value.value}`.replace(/ /g, '.'))
+      .map((className) => `.${className}`);
   }
 
-  private _extractId(elements: (JSXElement | JSXText)[]): void {
-    getPartialPropertyOfElements(elements).forEach(
-      ({ children, openingElement: { attributes } }) => {
-        attributes
-          .filter(isId)
-          .forEach((attr) => this._ids.push(`#${attr.value.value}`));
+  private _extractId(element: JSXElement): string[] {
+    const { openingElement } = element;
+    const { attributes } = openingElement;
 
-        this._extractId(children);
-      },
-    );
+    return attributes.filter(isId).map((attr) => `#${attr.value.value}`);
   }
-}
-
-function getFunctionDeclarations(
-  sources: ExportNamedDeclaration[],
-): FunctionDeclaration[] {
-  return sources
-    .map(({ declaration }) => declaration)
-    .filter(isFunctionDeclaration);
-}
-
-function getJSXElements(source: FunctionDeclaration[]): Expression[] {
-  return source
-    .map(({ body: blockStatement }) => blockStatement)
-    .flatMap(({ body }) => body)
-    .filter(isReturnStatement)
-    .map((returnStatement) => returnStatement.argument)
-    .filter(isNotNullAndUndefined);
-}
-
-function getPartialPropertyOfElements(
-  elements: (JSXElement | JSXText)[],
-): Pick<JSXElement, 'children' | 'openingElement'>[] {
-  return elements
-    .filter((element): element is JSXElement => isJSXElement(element))
-    .map(({ children, openingElement }) => {
-      return { children, openingElement };
-    });
 }
