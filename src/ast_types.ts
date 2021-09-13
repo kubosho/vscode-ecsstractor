a46bc085b2c10b38b24ff061546bf4f134c471d3
Shota Kubota <ta2@o2p.jp>
2021-09-13 22:49:21 +0900

    feat(ast_types): add ESTree types

---
+++ src/ast_types.ts
@@ -0,0 +1,8 @@
+import { TSESTree } from '@typescript-eslint/experimental-utils';
+
+export type JSXAttribute = TSESTree.JSXAttribute;
+export type JSXElement = TSESTree.JSXElement;
+export type JSXExpressionContainer = TSESTree.JSXExpressionContainer;
+export type Literal = TSESTree.Literal;
+export type ObjectExpression = TSESTree.ObjectExpression;
+export type Property = TSESTree.Property;
